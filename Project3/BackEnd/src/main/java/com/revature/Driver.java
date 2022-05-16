adf0407fa3b4f40b58e94768f52c7c56a511127b
Mark Martin <95247709+MarkMartin00@users.noreply.github.com>
2022-05-16 13:08:32 -0400

    m

---
+++ Project3/BackEnd/src/main/java/com/revature/Driver.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class Driver {
+    public static void main(String[] args) {
+        SpringApplication.run(Driver.class, args);
+    }
+}
